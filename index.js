e70c3139ee2e8a3143803e60a6c5a4bb4c6dfccc
Jose A Montero <80520012+Alberton1909@users.noreply.github.com>
2021-07-20 10:58:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+document.getElementsByClassName("download-button")[0].onclick = function preciosPage() {
+    location.href = 'http://127.0.0.1:5500/precios.html';
+   
+}
+
+document.getElementsByClassName("download-button")[1].onclick = function contacMePage() {
+    location.href = 'http://127.0.0.1:5500/contact-me.html';
+}
+
+document.getElementsByClassName("btn btn-secondary download-button")[0].onclick = function home() {
+    location.href = 'http://127.0.0.1:5500/index.html';
+
+    
+}
+
+
+document.getElementById("second-button").onclick = function (){
+    location.href = 'http://127.0.0.1:5500/precios.html';
+}
+
+
